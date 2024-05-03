029d87bd151b25c684c3c1e2594f1e431c98b2c0
Kruthi Prajapat <kp01166@surrey.ac.uk>
2024-05-03 22:38:52 +0100

    Added Vendor and Product model for database

--- models.py
+++ models.py
@@ -20,3 +20,23 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+class Vendor(db.Model):
+    __tablename__ = 'vendor'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(100))
+    location = db.Column(db.String(200))
+    description = db.Column(db.Text)
+    products = db.relationship('Product', backref='vendor', lazy='dynamic')
+    markets = db.relationship('Market', secondary=vendor_markets, back_populates="vendors")
+
+class Product(db.Model):
+    __tablename__ = 'product'
+    id = db.Column(db.Integer, primary_key=True)
+    vendor_id = db.Column(db.Integer, db.ForeignKey('vendor.id'), nullable=False)
+    name = db.Column(db.String(100))
+    price = db.Column(db.Float)
+    description = db.Column(db.Text)
+    availability = db.Column(db.Boolean, default=True)
+    contact_email = db.Column(db.String(120))  
+    contact_phone = db.Column(db.String(20))  
